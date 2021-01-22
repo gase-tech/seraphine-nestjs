cf2c35d01ece114b300b721c0cf361ca10af42ab
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2021-01-22 21:12:29 +0300

    Sentry integration

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -1,21 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, ErrorHandler, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { Router, RouterModule } from '@angular/router';
+import * as Sentry from '@sentry/angular';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
+    RouterModule.forRoot([{ path: '', component: AppComponent }]),
   ],
-  providers: [],
+  providers: [
+    {
+      provide: ErrorHandler,
+      useValue: Sentry.createErrorHandler({
+        showDialog: true,
+      }),
+    },
+    {
+      provide: Sentry.TraceService,
+      deps: [Router],
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: () => () => {},
+      deps: [Sentry.TraceService],
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -1,8 +1,26 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import * as Sentry from '@sentry/angular';
+import { Integrations } from '@sentry/tracing';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
+Sentry.init({
+  dsn:
+    'https://ba2d5fe2c0d548e5b283b6de8fa65b65@o320170.ingest.sentry.io/5604024',
+  autoSessionTracking: true,
+  integrations: [
+    new Integrations.BrowserTracing({
+      tracingOrigins: ['localhost', 'https://yourserver.io/api'],
+      routingInstrumentation: Sentry.routingInstrumentation,
+    }),
+  ],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
 
 if (environment.production) {
   enableProdMode();
@@ -10,4 +28,5 @@ if (environment.production) {
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
+  .then(() => console.log(`Bootstrap success`))
   .catch((err) => console.error(err));
